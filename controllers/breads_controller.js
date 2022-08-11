d8acbffa67571ac13f86f32e8ef09f1bd1358bf3
Ryan McGowan <vitomaserati@gmail.com>
2022-08-10 18:44:38 -0700

    delete route completed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -47,11 +47,19 @@ breads.get('/:arrayIndex', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
+})
+
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 
