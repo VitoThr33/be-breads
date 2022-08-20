0c46b52275d2e22b5eb69d3fe6dc22c687dbf53e
Ryan McGowan <vitomaserati@gmail.com>
2022-08-20 13:37:55 -0700

    add virtuals

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,66 +1,105 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+const Baker = require('../models/baker.js')
+const breadSeedData = require('../models/seed.js')
 
-// INDEX
+/// Index:
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread,
-        title: 'Index Page'
-      }
-    )
+  Baker.find()
+    .then(foundBakers => {
+      Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              bakers: foundBakers,
+              title: 'Index Page'
+          })
+      })
+    })
 })
-
 
 
 // NEW
 breads.get('/new', (req, res) => {
-  res.render('new')
+    Baker.find()
+      .then(foundBakers => {
+        res.render('new', {
+          bakers: foundBakers
+        })
+      })
 })
+
+// SHOW
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .populate('baker')
+      .then(foundBread => {
+        res.render('show', {
+            bread: foundBread
+        })
+      })
+      .catch(err => {
+        res.send('404')
+      })
+})
+
+// EDIT
+breads.get('/:id/edit', (req, res) => {
+  Baker.find()
+    .then(foundBakers => {
+        Bread.findById(req.params.id)
+          .then(foundBread => {
+            res.render('edit', {
+                bread: foundBread, 
+                bakers: foundBakers
+            })
+          })
+    })
+})
+
+// UPDATE
+breads.put('/:id', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
+})
+
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
-
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
-    })
-  } else {
-    res.render('404')
-  }
-})
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    })
-  } else {
-    res.render('404')
-  }
-})
-
-
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
 })
 
+// SEED ROUTE 
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany(breadSeedData)
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
 
 module.exports = breads
--- views/edit.jsx
+++ views/edit.jsx
@@ -1,37 +1,40 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function New ({bakers}) {
+function Edit ({bread, bakers}) {
     return (
       <Default>
-        <h2>Add a new bread</h2>
-        <form action="/breads" method="POST">
+        <h2>Edit a bread</h2>
+        <form action={`/breads/${bread.id}?_method=PUT`} method="POST">
           <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
             id="name"
             required
+            defaultValue={bread.name}
           />
           <label htmlFor="image">Image</label>
           <input
             type="text"
             name="image"
-            id="image"/>
-            <label htmlFor="baker">Baker</label>
-            <select name="baker" id="baker">
-              {bakers.map((baker) => {
-                return(
-                  <option value={baker.id} key={baker.id}>{baker.name}</option>
-                )
-              })}
-            </select>
+            id="image"
+            defaultValue={bread.image}
+          />
+          <label htmlFor="baker">Baker</label>
+          <select name="baker" id="baker" defaultValue={bread.baker}>
+            {bakers.map((baker) => {
+              return(
+                <option value={baker.id} key={baker.id}>{baker.name}</option>
+              )
+            })}
+          </select>
           <label htmlFor="hasGluten">Has Gluten?</label>
           <input
             type="checkbox"
             name="hasGluten"
             id="hasGluten"
-            defaultChecked
+            defaultChecked={bread.hasGluten}
           />
           <br />
           <input type="submit"/>
@@ -40,4 +43,4 @@ function New ({bakers}) {
     )
 }
 
-module.exports = New
+module.exports = Edit
